45b16005a10e47e0aeb73625ed7c7a6e0f90c954
tangh <suzhoutanghao@hotmail.com>
2019-02-02 15:22:30 +0800

    modify

--- tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
+++ tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
@@ -2,6 +2,7 @@ package com.tensquare.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @date 2019/2/2 0002 15:09
  */
 @EnableZuulProxy
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ManagerApplication {
 
     public static void main(String[] args) {
